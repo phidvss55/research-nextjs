be2dd08585a32379e6368993428529422a69fe9a
Phi Dinh <phidv9855@gmail.com>
2025-05-27 01:32:35 +0700

    add prisma with neon + sentry logging

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,16 +1,43 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
+import js from "@eslint/js";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
+const __dirname = path.dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+  baseDirectory: __dirname, // optional; default: process.cwd()
+  resolvePluginsRelativeTo: __dirname, // optional
+  recommendedConfig: js.configs.recommended, // optional unless you're using "eslint:recommended"
+  allConfig: js.configs.all, // optional unless you're using "eslint:all"
 });
 
-const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
-];
+export default [
+  ...compat.extends(
+    "plugin:react/recommended",
+    "plugin:@next/next/recommended"
+  ),
 
-export default eslintConfig;
+  ...compat.env({
+    es2020: true,
+    node: true,
+  }),
+
+  ...compat.plugins("jsx-a11y", "react"),
+
+  // translate an entire config
+  ...compat.config({
+    plugins: ["jsx-a11y", "react"],
+    extends: ["plugin:@next/next/recommended", "next"],
+    env: {
+      es2020: true,
+      node: true,
+    },
+    rules: {
+      semi: "error",
+      "no-unused-vars": "error",
+      "no-implicit-any": "error",
+    },
+  }),
+];
