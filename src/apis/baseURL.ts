eb299f251d8ea2370c677554232a8bc2bdb26634
Phi Dinh <phidv9855@gmail.com>
2025-05-26 21:54:02 +0700

    add code for sentry logging and neon pgsql

--- src/apis/baseURL.ts
+++
@@ -1,8 +0,0 @@
-const APIDomain =
-  process.env.REACT_APP_API_DOMAIN ||
-  "https://64351490537112453fcd07e0.mockapi.io/api/v1";
-
-const laneURL = `${APIDomain}/lane`;
-// const datatableURL = `${APIDomain}/datatable`;
-
-export { APIDomain, laneURL };
