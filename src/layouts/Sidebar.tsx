46f933ef9fb9ca7ea6006a34d839fce8000eab5a
Turbobot <turbobot@vercel.com>
2025-08-08 08:30:15 +0000

    feat(create-turbo): apply official-starter transform

--- src/layouts/Sidebar.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const Sidebar = () => {
-    return <div className="w-52 bg-purple-400 h-menu">Sidebar</div>;
-};
-
-export default Sidebar;
--- src/utils/lodash.ts
+++
@@ -1,8 +0,0 @@
-export function getValue<DataType, PathType extends string, DefaultType>(data: DataType, path: PathType, defaultValue?: DefaultType) {
-    const value = path
-        .split(/[.[\]]/)
-        .filter(Boolean)
-        .reduce((value, key) => (value as any)?.[key], data as any);
-
-    return value !== undefined ? value : (defaultValue as DefaultType);
-}
